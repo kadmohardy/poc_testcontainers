dd1630859c57bb1cefcbf4391c44fc09f17062a3
Kadmo Hardy <kadmohardy@CS0458.local>
2025-07-18 16:18:09 -0300

    chore: add logs

--- nginx.go
+++ nginx.go
@@ -2,6 +2,8 @@ package nginx
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/testcontainers/testcontainers-go"
@@ -15,14 +17,24 @@ type nginxContainer struct {
 
 func startContainer(ctx context.Context) (*nginxContainer, error) {
 	req := testcontainers.ContainerRequest{
-		Image:        "nginx",
+		Image:        "nginx:latest",
 		ExposedPorts: []string{"80/tcp"},
-		WaitingFor:   wait.ForHTTP("/").WithStartupTimeout(10 * time.Second),
+		WaitingFor:   wait.ForListeningPort("80/tcp").WithStartupTimeout(90 * time.Second),
 	}
 	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: req,
 		Started:          true,
 	})
+	if err == nil {
+		fmt.Println("Container logs:")
+		fmt.Print(err)
+
+	}
+	logs, err := container.Logs(ctx)
+	if err == nil {
+		defer logs.Close()
+		fmt.Println("Container logs:")
+	}
 	var nginxC *nginxContainer
 	if container != nil {
 		nginxC = &nginxContainer{Container: container}
@@ -31,10 +43,25 @@ func startContainer(ctx context.Context) (*nginxContainer, error) {
 		return nginxC, err
 	}
 
+	port, err := container.MappedPort(ctx, "80")
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Mapped port:", port.Port())
+
 	endpoint, err := container.PortEndpoint(ctx, "80", "http")
 	if err != nil {
 		return nginxC, err
 	}
+	fmt.Println("Endpoint:", endpoint)
+
+	ip, err := nginxC.Host(ctx)
+	if err != nil {
+		log.Printf("failed to create container: %s", err)
+		return nginxC, err
+	}
+	fmt.Println("IP port:", ip)
 
 	nginxC.URI = endpoint
 	return nginxC, nil
--- nginx_test.go
+++ nginx_test.go
@@ -1,29 +1,31 @@
 package nginx
 
 import (
-    "context"
-    "net/http"
-    "testing"
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
 
-    "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 
-    "github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go"
 )
 
 func TestIntegrationNginxLatestReturn(t *testing.T) {
-    if testing.Short() {
-        t.Skip("skipping integration test")
-    }
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
 
-    ctx := context.Background()
+	ctx := context.Background()
 
-    nginxC, err := startContainer(ctx)
-    testcontainers.CleanupContainer(t, nginxC)
-    require.NoError(t, err)
-
-    resp, err := http.Get(nginxC.URI)
-    require.NoError(t, err)
-    require.Equal(t, http.StatusOK, resp.StatusCode)
+	nginxC, err := startContainer(ctx)
+	testcontainers.CleanupContainer(t, nginxC)
+	require.NoError(t, err)
+	print("____________________________")
+	fmt.Print(nginxC)
+	fmt.Print(nginxC.URI)
+	print("____________________________")
+	resp, err := http.Get(nginxC.URI)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
 }
-
-Made with Material for MkDocs
